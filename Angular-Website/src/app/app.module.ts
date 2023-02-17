e5eec1bc1fc4cff07b5a35bfc8f5d5a66a66e121
Waleed <83013373+Hawxzie@users.noreply.github.com>
2023-02-17 18:25:54 +0800

    Adding components

--- Angular-Website/src/app/app.module.ts
+++ Angular-Website/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    SearchbarComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
